dc1c29e14514bcaaa6357107bbc8222e6cc11535
shrutiparsodkar02 <2023bit507sggs.ac.in>
2024-04-08 17:08:43 +0530

    changes in library management code

--- libraryManagement.java
+++ libraryManagement.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class LibraryManagement{
+class Person{
 	String name;
 	int year;
 	long mobile_num;
@@ -17,8 +17,26 @@ class LibraryManagement{
 		this.year=year;
 	}
 }
-class Librarian{
+/*class Faculty extends Person{
+	String dept_of_faculty;
+	void getdept_of_faculty(String dept_of_faculty){
+		this.dept_of_faculty=dept_of_faculty;	
+	}
+	void displayDetails_of_faculty(){
+		System.out.println("Displaying faculty details---->")
+		System.out.println("name entered is---->"+name);
+		
+		System.out.println("mobile num entered is---->"+mobile_num);
+	}
 	
+
+
+}*/
+class Librarian{
+	void displayLibrarianDetails(String name,long mob_num){
+		System.out.println("librarain is--->"+name);
+		System.out.println("mobile num of librarian is--->"+mob_num);
+	}
 	boolean isAvailable(String[] arr,String name_bookname){
 		int i=0;
 		for(i=0;i<arr.length;i++){
@@ -34,34 +52,37 @@ class Librarian{
 	}
 	String[] issueBook(String[] arr,String name_bookname){
 	
-		String[] arr_new=new String[arr.length-1];
-		
+		String[] arr_new=new String[(arr.length)];
+		/*for(int k=0;k<arr.length;k++){
+			arr_new[k]=arr[k];
+		}
+		for(String book:arr_new ){
+			System.out.println(book+" ");
+			}
 		System.out.println("Avaialble Books::");
 		for(String book: arr){
 			System.out.println(book+" ");
-		}
+		}*/
 		
 		if(isAvailable(arr,name_bookname)==true){
 			for(int i = 0; i < arr.length; i++){
 		     	 	if(arr[i].equals(name_bookname)==true) {
-			// shifting elements
-				
-				//System.out.println("inside");
-					for(int j = i+1; j < arr.length - 1; j++){
+			// shifting elements System.out.println("inside");
+					for(int j = i; j < arr.length-1; j++){
 						arr_new[j] = arr[j+1];
-						System.out.print(arr_new[j]);
+						System.out.print(arr_new[j]+" ");
 					}
-					arr_new[arr.length-2]="NA";
+			//System.out.println("\n\n");
+					arr_new[arr.length-1]="NA";
 					break;
-			 	}
-			 	
-		    	}
+				}
+			}
 			//System.out.println("Before deletion :" + Arrays.toString(arr));
 			//System.out.println("After deletion :" + Arrays.toString(arr_new));
 			/*for(String book:arr_new ){
 			System.out.println(book+" ");
-			}*/
-			
+			}
+			*/
 			System.out.println("book is issued");
 			return arr_new;	
 		}
@@ -69,6 +90,27 @@ class Librarian{
 			System.out.println("book not issued");
 			return arr;
 		}
+	}
+	String[] returnBook(String[] arr,String name_bookname){
+		//int length=(arr.length)+1 ;
+		//String[] after_book_returned=new String[length];
+		//int index=arr.length-1;
+		//now we will shift elements
+		/*
+		
+		*/
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]=="NA"){
+				arr[i]=name_bookname;
+			}
+		
+		}
+		System.out.println("book is returned");
+		//after_book_returned[length-1]=name_bookname;
+		/*for(String book: arr ){
+			System.out.println(book+" ");
+		}*/
+	return arr;
 	}
 	
 		
@@ -104,7 +146,7 @@ class Book{
 	}
 }
 
-class Student extends LibraryManagement{
+class Student extends Person{
 	String regNum;
 	void getRegistrationNum(String regnum){
 		this.regNum=regnum;
@@ -117,14 +159,19 @@ class Student extends LibraryManagement{
 		System.out.println("year entered is---->"+year);
 		System.out.println("mobile num entered is---->"+mobile_num);
 	}
+	
+	
+}
+class MainBody{
+	
 	public static void main(String args[]){
 		Student s1= new Student();
-		LibraryManagement l1=new LibraryManagement();
+		Person l1=new Person();
 		Librarian l=new Librarian();
 		Book b= new Book();
 		Scanner sc= new Scanner(System.in);
 		
-		
+		System.out.println("<---WELCOME TO LIBRARY MANAGEMENT SYSTEM--->");
 		System.out.println("enter name");
 		String name1=sc.nextLine();
 		s1.getName(name1);
@@ -149,7 +196,7 @@ class Student extends LibraryManagement{
 		s1.getMobNum(mobnum);
 		//System.out.println("\n\n");
 		
-		s1.displayDetails();
+		//s1.displayDetails();
 		
 		
 		System.out.println("enter book name");
@@ -170,7 +217,7 @@ class Student extends LibraryManagement{
 
 		System.out.println("\n\n");
 		
-		b.displayBookDInfo();
+		/*b.displayBookDInfo();
 		String[] books={"java","python","c++","c","maths fundamentals","data structure"};
 		//b.displayArray(books);
 		//l.isAvailable(books,bookname);
@@ -179,7 +226,62 @@ class Student extends LibraryManagement{
 		System.out.println("\n\n");
 		//l.isAvailable(after_book_issue,bookname);
 		
+		System.out.println("enter the book which you want the return");
+		String return_book=sc.nextLine();
+		System.out.println("\n\n");
+		l.returnBook(after_book_issue,return_book);*/
+		//l.displayLibrarianDetails("R.Raman",784589541);
+		String[] books={"java","python","c++","c","maths fundamentals","data structure"};
+		 while (true) {
+	 		System.out.println("\n\n");
+		    System.out.println("1.show student details ");
+		    System.out.println("2.Show book details");
+		    System.out.println("3.check book is avilable");
+		    System.out.println("4. Isuuse book ");
+		    System.out.println("5. Return book");
+		   /* System.out.println("6. ");
+		    System.out.println("7. ");
+		    System.out.println("8. ");*/
+		    System.out.println("Enter your choice:");
+		   int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Student details are--->");
+                    s1.displayDetails();
+                    System.out.println("\n\n");
+                    break;
+                case 2:
+                    	System.out.println("all book related details are --->");
+                    	b.displayBookDInfo();
+                    	System.out.println("\n\n");
+                    break;
+                case 3:
+                	System.out.println("librarian details are-->");
+                	l.displayLibrarianDetails("R.Raman",784589541);
+                   System.out.println("Status of book avilable or not-->" + l.isAvailable(books,bookname));
+                   System.out.println("\n\n");
+                    break;
+                case 4:
+                   	l.issueBook(books,bookname);
+                   	//String[] after_book_issue=l.issueBook(books,bookname);
+                   	System.out.println("\n\n");
+                   	break;
+                case 5:
+                    	//l.issueBook(books,bookname);
+		        System.out.println("enter the book which you want the return");
+		   	Scanner sc1= new Scanner(System.in);
+			String return_book=sc1.nextLine();
+			System.out.println("\n\n");
+	               	String[] after_book_issue1=l.issueBook(books,bookname);
+			l.returnBook(after_book_issue1,return_book);
+			System.out.println("\n\n");
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+            }
+		
 	}
-	
-}
+	}
 
+
+}
